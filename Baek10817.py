93094e6da1937c29fd51c38b1746162fd21d3750
jjeongho <jakchung1021@gmail.com>
2020-07-20 18:00:25 +0900

    Baek10817

---
+++ Baek10817.py
@@ -0,0 +1,10 @@
+import sys
+num = list(map(int, sys.stdin.readline().split()))
+sum = 0
+
+for i in range(3):
+    for j in range(2):
+        if num[j] > num[j + 1]:
+            num[j], num[j + 1] = num[j + 1], num[j]
+    print(num)
+
