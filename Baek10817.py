4c8944d3b6860ff090441654daff26b4588af4db
jjeongho <jakchung1021@gmail.com>
2020-07-22 14:32:28 +0900

    Baek2523

--- Baek10817.py
+++ Baek10817.py
@@ -1,4 +1,7 @@
 import sys
+
+
+def bubblesort()
 num = list(map(int, sys.stdin.readline().split()))
 sum = 0
 
@@ -6,5 +9,6 @@ for i in range(3):
     for j in range(2):
         if num[j] > num[j + 1]:
             num[j], num[j + 1] = num[j + 1], num[j]
-    print(num)
+            sum = num
+
 
---
+++ Baek2523.py
@@ -0,0 +1,9 @@
+star = 0
+
+star = int(input())
+
+for i in range(1, star+1):
+    print("*"*i)
+
+for j in range(star-1,0,-1):
+    print("*"*j)
